751022c692dde4971e5308e00eef660696432a6c
Swarnendu Bhandari <94161758+Swarnendu0123@users.noreply.github.com>
2024-03-02 21:47:52 +0530

    email added

--- client/src/Routes/Landing.jsx
+++ client/src/Routes/Landing.jsx
@@ -124,7 +124,7 @@ const Landing = () => {
                                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white w-60">Password</label>
                                 <input type="password" id="in_password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="password" required />
                             </div>
-                            <button href="#" onClick={SignIn} className=" text-white dark:text-black bg-blue-600 dark:bg-blue-500 hover:underline font-medium text-lg inline-flex items-center">Sign In
+                            <button href="#" onClick={SignIn} className="p-4 text-white dark:text-black bg-blue-600 dark:bg-blue-500 hover:underline font-medium text-lg inline-flex items-center">Sign In
                                 <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                                 </svg>
@@ -167,7 +167,7 @@ const Landing = () => {
                                 ))}
                             </select>
                             </div>
-                            <button href="#" onClick={SignUp} className=" text-white dark:text-black bg-blue-600 dark:bg-blue-500 hover:underline font-medium text-lg inline-flex items-center">Sign Up
+                            <button href="#" onClick={SignUp} className="p-4 text-white dark:text-black bg-blue-600 dark:bg-blue-500 hover:underline font-medium text-lg inline-flex items-center">Sign Up
                                 <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                                 </svg>
