8bc0d0319d496eacca3ab12fb2da8ac67f48e600
Swarnendu <94161758+Swarnendu0123@users.noreply.github.com>
2024-05-09 19:19:51 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const NavBar = () => {
             <nav className="bg-white border-gray-200 dark:bg-gray-900">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a href="/auth" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Breath Recomendation System</span>
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Breadth Recomendation System</span>
                     </a>
                     <div className="flex md:order-2">
                         <div className="flex items-center space-x-4 p-3">
